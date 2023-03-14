ff4171a93e8c5b9a469a588f003443248ff575e2
David-TC13 <115069379+David-TC13@users.noreply.github.com>
2023-03-14 21:36:34 +0100

    uploading

--- src/wordcloud.py
+++ src/wordcloud.py
@@ -3,45 +3,24 @@
 
 # In[2]:
 
-
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 import pandas as pd
-
+from wordcloud import WordCloud
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 # In[3]:
 
-
-df_bbc_russia=pd.read_csv('data/processed/bbc_russia_proc.csv')
-df_bbc_china=pd.read_csv('data/processed/bbc_china_proc.csv')
-df_cnn_russia=pd.read_csv('data/processed/cnn_russia_proc.csv')
-df_cnn_china=pd.read_csv('data/processed/cnn_china_proc.csv')
-pdo_uk=pd.read_csv('data/processed/pdo_united_kingdom_proc.csv')
-pdo_usa=pd.read_csv('data/processed/pdo_united_states_proc.csv')
-rt_uk=pd.read_csv('data/processed/rt_united_kingdom_proc.csv')
-rt_usa=pd.read_csv('data/processed/rt_united_states_proc.csv')
-
-
-# In[4]:
-
-
-def wordcloud(df):
+def wordcloud(df,file):
+    """This function creates a wordcloud of a specific DF and with a specific shape"""
     words=[word for txt in df['word'] for word in txt]
     text = ' '.join(words)
-    wordcloud = WordCloud(width=1000, height=800, background_color='white').generate(text)
+    mask = np.array(Image.open(f"pics/{file}"))
+    wordcloud = WordCloud(width=1000, height=800, background_color='beige',mask=mask,contour_width=1, contour_color='gray').generate(text)
     plt.figure(figsize=(8,10))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
+    plt.savefig(f'pics/wordcloud_{file}', dpi=300, bbox_inches='tight')
     return plt.show()
 
-# In[5]:
-
-
-
-
-
 # In[ ]:
-
-
-
-
