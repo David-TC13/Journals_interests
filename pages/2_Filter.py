6e0c1f28f564eceb994b62e2109903bb6b931324
David-TC13 <115069379+David-TC13@users.noreply.github.com>
2023-03-21 23:48:40 +0100

    uploading

--- pages/2_Filter.py
+++ pages/2_Filter.py
@@ -29,16 +29,9 @@ day = st.selectbox('day', df3['day'].unique())
 df4=df3[(df3['day'] == day)]
 title= st.selectbox('Title', df4['title'])
 df5= df4[df4['title']==title]
-
 link=df5['link'].values[0]
+st.write(df5.loc[:,['subjectivity','polarity','word','source','topic']])
+
 if st.button("Read full article"):
     js = "window.open('{}')".format(link)
     components.html('<script>{}</script>'.format(js))
-
-
-html = f'<iframe src="{link}" width="1000" height="600" allow="autoplay; fullscreen" allowfullscreen></iframe>'
-st.write(html, unsafe_allow_html=True)
-
-st.write(df5.loc[:,['subjectivity','polarity','word','source','topic']])
-components.iframe(link,scrolling=True, height=500)
-
